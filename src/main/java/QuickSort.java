29a9daf87de9fc00abf3b097e9545eab15db1013
Jacob Logsdon <jrlogsdon21@gmail.com>
2021-12-01 19:30:04 -0500

    adding in QuickSort

--- src/main/java/QuickSort.java
+++ src/main/java/QuickSort.java
@@ -1,7 +1,44 @@
 public class QuickSort {
 
     public static void sort(int[] nums) {
+        QuickSort quickSort = new QuickSort();
+        int startIndex = 0;
+        int endIndex = nums.length - 1;
 
+        quickSort.quickSort(nums, startIndex, endIndex);
+    }
+
+    private void quickSort(int[] nums, int startIndex, int endIndex) {
+        if (startIndex < endIndex) {
+            int partition = partition(nums, startIndex, endIndex);
+
+            quickSort(nums, startIndex, partition - 1);
+
+            quickSort(nums, partition + 1, endIndex);
+        }
 
     }
+
+    private int partition(int[] nums, int startIndex, int endIndex) {
+        int pivot = nums[endIndex];
+
+        for (int j = startIndex; j <= endIndex - 1; j++) {
+
+            if (nums[j] < pivot) {
+                swap(nums, startIndex, j);
+                startIndex++;
+            }
+
+        }
+        swap(nums, startIndex, endIndex);
+        return startIndex;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
 }
--- src/test/java/QuickSortTest.java
+++ src/test/java/QuickSortTest.java
@@ -5,10 +5,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class QuickSortTest {
 
     @Test
-    void canCallSort() {
-        int[] nums = new int[]{4,3,1,2};
+    void sortBasic() {
+        int[] nums = new int[]{4, 3, 1, 2};
+        int[] expected = new int[]{1, 2, 3, 4};
         QuickSort.sort(nums);
+        assertArrayEquals(expected, nums);
+    }
 
+    @Test
+    void sortDuplicates() {
+        int[] nums = new int[]{6, 6, 3, 1, 0, -1, 2, -5};
+        int[] expected = new int[]{-5, -1, 0, 1, 2, 3, 6, 6};
+
+        QuickSort.sort(nums);
+        assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    void sortLargeInput() {
+        int[] nums = new int[] {15, 13, 12, 14, -1, 2, 6, 8, 11, 14, 13, 12, 1, 3, 3, 64, -100};
+        int[] expected = new int[] {-100, -1, 1, 2, 3, 3, 6, 8, 11, 12, 12, 13, 13, 14, 14, 15, 64};
+
+        QuickSort.sort(nums);
+        assertArrayEquals(expected, nums);
     }
 
 }
